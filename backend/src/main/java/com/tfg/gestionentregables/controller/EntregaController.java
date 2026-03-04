94e9bc64c57c2e2599058e438bc2792fa3f8750c
Mancalrod <mancalrod@alum.us.es>
2026-03-04 14:26:16 +0100

    fix: Arreglando workflow

--- backend/src/main/java/com/tfg/gestionentregables/controller/EntregaController.java
+++ backend/src/main/java/com/tfg/gestionentregables/controller/EntregaController.java
@@ -15,7 +15,6 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
-import java.net.MalformedURLException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
@@ -112,7 +111,7 @@ public class EntregaController {
                                         "attachment; filename=\"" + material.getNombre() + "\"")
                                 .body(resource.getContentAsByteArray());
                     }
-                } catch (MalformedURLException | IOException ex) {
+                } catch (IOException ex) {
                     // ignorar
                 }
             }
